56ad3591eea1e22fa2d60d388799586a9440f593
Corri Golden <corri.golden@gmail.com>
2019-12-15 08:37:14 -0600

    finished login

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom'
+import { Route, withRouter, Redirect } from 'react-router-dom'
 import React, { Component } from 'react'
 import Home from './home/Home.js'
 import KeepUp from './KeepUp.js'
@@ -6,6 +6,8 @@ import HomeDetail from './home/HomeDetail.js';
 import Tickets from './Tickets/Tickets.js'
 import TicketList from './Tickets/TicketList'
 import HomeDetailEdit from './home/HomeDetailEdit'
+import Login from './Auth/Login.js';
+
 
 
 
@@ -15,20 +17,33 @@ import HomeDetailEdit from './home/HomeDetailEdit'
 
 class ApplicationViews extends Component {
 
-    render() {
+    isAuthenticated = () => localStorage.getItem("credentials") !== null
+
+    render() {  //i'm not using the keepUp.  It is home.
         return (
             <React.Fragment>
+                <Route path="/login" component={Login} />
+                    }}
+                />
                 <Route exact path="/" render={(props) => {
-                    return <Home />
+                    if (this.isAuthenticated()) {
+                        return <Home {...props} />
+                    } else {
+                        return <Redirect to="/login" />
+                    }
                 }} />
-                <Route path="/keepUp" render={(props) => {
+                /* <Route path="/keepUp" render={(props) => {
                     return <KeepUp />
-                }} />
+                }} /> */
                 <Route path="/homeDetail" render={(props) => {
                     return <HomeDetail {...props} />
                 }} />
                 <Route path="/tickets" render={(props) => {
-                    return <TicketList {...props} />
+                    if (this.isAuthenticated()) {
+                        return <Home {...props} />
+                    } else {
+                        return <Redirect to="/login" />
+                    }
                 }} />
                 <Route
                     path="/tickets/:messageId(\d+)/edit" render={props => {
