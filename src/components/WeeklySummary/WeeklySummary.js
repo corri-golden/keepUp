1faca4bbd9c76462e907a39e9d79467641e50a3b
Corri Golden <corri.golden@gmail.com>
2020-01-03 07:26:08 -0600

    finished the styling for weekly summary edit

--- src/components/WeeklySummary/WeeklySummary.js
+++ src/components/WeeklySummary/WeeklySummary.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Col, Container, Row, Card, Image, Button, Form } from "react-bootstrap"
+import { Col, Container, Row, Card, Image, Button, Form, ListGroup } from "react-bootstrap"
 import { getUser } from '../modules/Helper.js';
 
 
@@ -8,24 +8,24 @@ import { getUser } from '../modules/Helper.js';
 class WeeklySummary extends Component {
     render() {
         return (
-            <Card width="100%" className="shadow-lg p-3 mb-5 bg-white rounded">
-               <Card.Text border="secondary">{this.props.weeklySummary.user.userName}</Card.Text>
-                <Card.Header><h1>{this.props.weeklySummary.date}</h1></Card.Header>
-                <Card.Title><h3>{this.props.weeklySummary.car.carMake} {this.props.weeklySummary.car.carModel}</h3></Card.Title>
-                <Card.Body>
-                <Card.Text border="secondary" bg="primary" rows="3"><h5>Mileage:{this.props.weeklySummary.mileage}</h5></Card.Text>
-                <Card.Text border="secondary" bg="primary" rows="3">{this.props.weeklySummary.recommendations}</Card.Text>
-                </Card.Body>
-                <Button variant="danger" onClick={() => this.props.deleteWeeklySummary(this.props.weeklySummary.id)} size="lg">
-                            Delete
+            <Card className="mt-3" className="shadow-lg p-3 mb-5 bg-white rounded">
+            <Card.Text border="secondary">{this.props.weeklySummary.user.userName}</Card.Text>
+            <Card.Header><h2>Weekly Summary</h2></Card.Header>
+            <ListGroup variant="flush" />
+                <ListGroup.Item><h4>Date: {this.props.weeklySummary.date}</h4></ListGroup.Item>
+                <ListGroup.Item><h4>Car: {this.props.weeklySummary.car.carMake} {this.props.weeklySummary.car.carModel}</h4></ListGroup.Item>
+                <ListGroup.Item text=""><h4>Mileage: {this.props.weeklySummary.mileage}</h4></ListGroup.Item>
+                <ListGroup.Item rows="8" border=""><h5>{this.props.weeklySummary.recommendations}</h5></ListGroup.Item>
+        
+                    <Button variant="danger" onClick={() => this.props.deleteWeeklySummary(this.props.weeklySummary.id)} size="lg">
+                    Delete
                             </Button>
                 <Button border="primary" className="mt-3" variant="warning" onClick={() => { this.props.history.push(`/weeklySummaries/${this.props.weeklySummary.id}/edit`) }} size="lg">Edit</Button>
-
 
             </Card>
         )
     }
 }
- 
+
 
 export default WeeklySummary
