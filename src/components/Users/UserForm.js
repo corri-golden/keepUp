5e320aba86390132c2cf72a7a1b975ee7c6c50f2
Corri Golden <corri.golden@gmail.com>
2020-01-03 22:22:15 -0600

    login for admin finished

--- src/components/Users/UserForm.js
+++ src/components/Users/UserForm.js
@@ -12,6 +12,7 @@ class UserForm extends Component {
         userName: "",
         password: "",
         id: "",
+        isAdmin: false,
     };
 
     handleFieldChange = evt => {
@@ -24,12 +25,13 @@ class UserForm extends Component {
         evt.preventDefault();
         if (this.state.userName === "") {
             window.alert("Fill Out a Recommendations");
-        // } else if (Number(this.state.id) === 0) {
-        //     window.alert("Add UserId");
+            // } else if (Number(this.state.id) === 0) {
+            //     window.alert("Add UserId");
         } else {
             this.setState({ loadingStatus: true });
             const user = {
                 userName: this.state.userName,
+                isAdmin: false,
                 password: this.state.password,
                 id: Number(this.state.userId),
                 // userId: getUser().id,
@@ -37,47 +39,52 @@ class UserForm extends Component {
             // Create the message and redirect user to ticket 
             usersManager.post(user)
                 .then(() => this.props.history.push("/users"))
-                console.log("yop", user)
+            console.log("yop", user)
         }
     };
 
 
     componentDidMount() {
         usersManager.getAllUsers()
-        .then(users => {
-            console.log(users)
-            this.setState({users: users})
-        }
-        )}  
-        render() {
-            return (
-                <form>
+            .then(users => {
+                console.log(users)
+                this.setState({ users: users })
+            }
+            )
+    }
+    render() {
+        return (
+            <form>
                 <Form.Group>
-                <Form.Control className="mt-3" onChange={this.handleFieldChange} type="userName"
-                  id="userName"
-                  placeholder="User Name"
-                  required="" autoFocus="" />
-                
+                    <Form.Control className="mt-3" onChange={this.handleFieldChange} type="userName"
+                        id="userName"
+                        placeholder="User Name"
+                        required="" autoFocus="" />
+
                 </Form.Group>
                 <Form.Group>
-                <Form.Control className="mt-0" onChange={this.handleFieldChange} type="password"
-                  id="password"
-                  placeholder="Password"
-                  required="" />
-               
+                    <Form.Control className="mt-0" onChange={this.handleFieldChange} type="password"
+                        id="password"
+                        placeholder="Password"
+                        required="" />
+
                 </Form.Group>
                 <Form.Group>
-                <Form.Control onChange={this.handleFieldChange} type="password"
-                  id="confirmPass"
-                  placeholder="Confirm Password"
-                  required="" />
-                <label htmlFor="inputPassword"></label>
+                    <Form.Control onChange={this.handleFieldChange} type="password"
+                        id="confirmPass"
+                        placeholder="Confirm Password"
+                        required="" />
+                    <label htmlFor="inputPassword"></label>
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check value="false" type="checkbox" label="Administrator" />
+                    
                 </Form.Group>
                 <Button className="center" type="submit" variant="warning" disabled={this.state.loadingStatus} onClick={this.constructNewUser} block>Create New Account
                 </Button>
-              </form>
-            )
-        }
+            </form>
+        )
+    }
 
 }
 
